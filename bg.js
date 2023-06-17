447ea613925b50a32be0afe3af335d07b9860ec5
jianghong <jianghong2020@qq.com>
2023-06-17 14:23:14 +0800

    update bg.js

--- bg.js
+++ bg.js
@@ -7,13 +7,13 @@
 //公共数据
 const sys_conf = 'sys_conf';
 const data = {
-    active_tab:{},
-    auto_download:0,
-    path:"\.(mp3|mp4|wav|ogg|m4a|aac|mpeg|m3u8)",
-    el_selector:'',
-    file_type:'',
-    choose:0,
-    version:'1.0'
+    active_tab: {},
+    auto_download: 0,
+    path: "\.(mp3|mp4|wav|ogg|m4a|aac|mpeg|m3u8)",
+    el_selector: '',
+    file_type: '',
+    choose: 0,
+    version: '1.0'
 };
 
 chrome.runtime.onInstalled.addListener(() => {
@@ -39,16 +39,16 @@ chrome.runtime.onInstalled.addListener(() => {
  * 设置元素选择模式，更新菜单
  * @param {string} mid 
  */
-function switchChooseMode(mid=''){
-    data.choose = data.choose?0:1;
+function switchChooseMode(mid = '') {
+    data.choose = data.choose ? 0 : 1;
     let title = 'media helper【元素选择】';
-    if(data.choose===1){
+    if (data.choose === 1) {
         title = 'media helper【停止选择】';
     }
-    chrome.contextMenus.update(mid,{
+    chrome.contextMenus.update(mid, {
         title
     });
-    sendMessageToActive(data.choose,'switchChooseMode')
+    sendMessageToActive(data.choose, 'switchChooseMode')
 }
 
 //数据监听
@@ -56,7 +56,8 @@ chrome.runtime.onMessage.addListener((m, s) => {
     const tab = s.tab;
     switch (m.cmd) {
         case "conf":
-            cache_set(sys_conf,m.data)
+            console.log(132)
+            cache_set(sys_conf, m.data)
             break
         case "keep":
             console.log('保持连接')
@@ -69,8 +70,8 @@ chrome.runtime.onMessage.addListener((m, s) => {
  * @param {string} key 
  * @param {object} val 
  */
-function cache_set(key,val){
-    if(!val){
+function cache_set(key, val) {
+    if (!val) {
         return
     }
     const obj = {};
@@ -92,20 +93,20 @@ function cache_get(key, callback) {
  */
 chrome.webRequest.onResponseStarted.addListener(
     function (details) {
-        let reg = new RegExp(data.path,'ig');
+        let reg = new RegExp(data.path, 'ig');
         if (details.type === data.file_type || !data.file_type || data.file_type === 'all') {
             let temp = details.url.match(reg);
-            
-            if(!temp && !data.path){
+
+            if (!temp && !data.path) {
                 return { cancel: false };
             }
-            
+
             sendMessageToActive({
-                type: data.file_type?data.file_type:'all',
+                type: data.file_type ? data.file_type : 'all',
                 requestId: details.requestId,
                 url: details.url,
-                data:details
-            },'resource')
+                data: details
+            }, 'resource')
         }
         return { cancel: false };
     },
@@ -118,11 +119,13 @@ chrome.webRequest.onResponseStarted.addListener(
  */
 function sendMessageToActive(msg = {}, cmd = 'msg') {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        if(!tabs[0]){
+        if (!tabs[0] && !tabs[0].id) {
             return
         }
         chrome.tabs.sendMessage(tabs[0].id, {
-            data:msg, cmd
+            data: msg, cmd
+        }).catch(err => {
+            console.log(err)
         })
     })
 }
@@ -131,10 +134,10 @@ function sendMessageToActive(msg = {}, cmd = 'msg') {
 /**
  * 加载配置
  */
-cache_get(sys_conf,(res)=>{
-    if(res[sys_conf]){
-        for (let k in res[sys_conf]){
-            if(typeof res[sys_conf][k] === 'string'){
+cache_get(sys_conf, (res) => {
+    if (res[sys_conf]) {
+        for (let k in res[sys_conf]) {
+            if (typeof res[sys_conf][k] === 'string') {
                 data[k] = res[sys_conf][k];
             }
 
